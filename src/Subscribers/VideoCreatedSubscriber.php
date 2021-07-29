32c52ff3f9db110a123d7a5ff3ac3a292912a082
Abdelhaq KHARROU <abdelhaq.fste@gmail.com>
2021-07-29 23:43:19 +0100

    handling one event kernel.Response with 2 methods with different priorities

--- src/Subscribers/VideoCreatedSubscriber.php
+++ src/Subscribers/VideoCreatedSubscriber.php
@@ -3,6 +3,9 @@
 namespace App\Subscribers;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
 
 class VideoCreatedSubscriber implements EventSubscriberInterface
 {
@@ -12,10 +15,27 @@ class VideoCreatedSubscriber implements EventSubscriberInterface
         dump('Subscriber called!');
     }
 
+    public function onKernelResponse1(ResponseEvent $event)
+    {
+        $response = new Response('Kernel event catched by Subscriber <br> onKernelEvent1 event executed');
+        dump('onKernelResponse1');
+        $event->setResponse($response);
+    }
+
+    public function onKernelResponse2(ResponseEvent $event)
+    {
+        $response = new Response('Kernel event catched by Subscriber <br> onKernelEvent2 event executed');
+        dump('onKernelResponse2');
+        $event->setResponse($response);
+    }
+
     public static function getSubscribedEvents()
     {
         return [
             'video.created.event' => 'onVideoCreatedEvent',
+            KernelEvents::RESPONSE => [
+                ['onKernelResponse2', 1],
+                ['onKernelResponse1', 2], ],
         ];
     }
 }
